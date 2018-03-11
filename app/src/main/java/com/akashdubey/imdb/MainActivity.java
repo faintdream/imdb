3b29f5c0b02d1ea39aca253f0a0e129971b27bbe
faintdream <dubey.akash@gmail.com>
2018-03-11 11:41:15 +0530

    get - Popular Movies, Upcoming movies, Now PLaying movies, Top rated movies functional now.

--- app/src/main/java/com/akashdubey/imdb/MainActivity.java
+++ app/src/main/java/com/akashdubey/imdb/MainActivity.java
@@ -2,21 +2,85 @@ package com.akashdubey.imdb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
-
-import com.akashdubey.imdb.model.MovieModel;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 import com.akashdubey.imdb.network.MyWebService;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     static public RecyclerView recyclerView;
+    MyWebService myWebService ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        recyclerView=findViewById(R.id.recyclerview1);
-        MyWebService myWebService=new MyWebService();
+        recyclerView = findViewById(R.id.recyclerview1);
+        myWebService= new MyWebService();
         myWebService.getUpcomingMovies();
+
     }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.in_app_choices, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        switch (itemId) {
+            case R.id.itemFavourite:
+                break;
+            case R.id.itempWatchlist:
+                break;
+            case R.id.itemRefresh:
+                break;
+
+            case R.id.itemMostPopularMovies:
+                Toast.makeText(this, "Popular movies", Toast.LENGTH_SHORT).show();
+                myWebService.getMostPopularMovies();
+                break;
+
+            case R.id.itemUpcomingMovies:
+                Toast.makeText(this, "Upcoming movies", Toast.LENGTH_SHORT).show();
+                myWebService.getUpcomingMovies();
+                break;
+
+            case R.id.itemLatestMovies:
+                Toast.makeText(this, "Latest Movies", Toast.LENGTH_SHORT).show();
+                myWebService.getLatestMovies();
+                break;
+
+            case R.id.itemNowPlayingMovies:
+                Toast.makeText(this, "Now Playing movies", Toast.LENGTH_SHORT).show();
+                myWebService.getNowPlayingMovies();
+                break;
+            case R.id.itemTopRatedMovies:
+                Toast.makeText(this, "Top rated movies", Toast.LENGTH_SHORT).show();
+                myWebService.getTopRatedMovies();
+                break;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
 }
--- app/src/main/java/com/akashdubey/imdb/network/MyWebService.java
+++ app/src/main/java/com/akashdubey/imdb/network/MyWebService.java
@@ -29,30 +29,35 @@ import static com.akashdubey.imdb.model.MovieModel.movieModelList;
  */
 
 public class MyWebService {
-    public static final String VOTE_COUNT="vote_count";
-    public static final String VOTE_AVERAGE="vote_average";
-    public static final String POPULARITY="popularity";
-    public static final String MOVIE_IMAGE="poster_path";
-    public static final String MOVIE_TITLE="title";
-    public static final String RELEASE_DATE="release_date";
+    public static final String URL_UPCOMING_MOVIES =
+            "http://api.themoviedb.org/3/movie/upcoming?api_key=8496be0b2149805afa458ab8ec27560c";
+    public static final String URL_POPULAR_MOVIES =
+            "http://api.themoviedb.org/3/movie/popular?api_key=8496be0b2149805afa458ab8ec27560c";
+    public static final String URL_NOW_PALYING_MOVIES =
+            "http://api.themoviedb.org/3/movie/now_playing?api_key=8496be0b2149805afa458ab8ec27560c";
+    public static final String URL_LATEST_MOVIES =
+            "http://api.themoviedb.org/3/movie/latest?api_key=8496be0b2149805afa458ab8ec27560c";
+    public static final String URL_TOP_RATED_MOVIES =
+            "http://api.themoviedb.org/3/movie/top_rated?api_key=8496be0b2149805afa458ab8ec27560c";
+
+    public static final String VOTE_COUNT = "vote_count";
+    public static final String VOTE_AVERAGE = "vote_average";
+    public static final String POPULARITY = "popularity";
+    public static final String MOVIE_IMAGE = "poster_path";
+    public static final String MOVIE_TITLE = "title";
+    public static final String RELEASE_DATE = "release_date";
 
 
-    MainActivity mainActivity=new MainActivity();
-    OkHttpClient okHttpClient=new OkHttpClient();
+    public MainActivity mainActivity = new MainActivity();
+    OkHttpClient okHttpClient = new OkHttpClient();
     Request request;
     JSONObject jsonObject;
     JSONArray jsonArray;
-//    private MovieModel movieModel;
 
-
-    void getMostPopularMovies(){
-        String url="";
-    }
-
-    public void getUpcomingMovies(){
-
-        String url="http://api.themoviedb.org/3/movie/upcoming?api_key=8496be0b2149805afa458ab8ec27560c";
-        request= new Request.Builder().url(url).build();
+    public void getMostPopularMovies() {
+        movieModelList.clear();
+        String url =URL_POPULAR_MOVIES;
+        request = new Request.Builder().url(url).build();
         okHttpClient.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
@@ -62,21 +67,71 @@ public class MyWebService {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
 
-                String myResponse=response.body().string().toString();
+                String myResponse = response.body().string().toString();
                 try {
-                    jsonObject= new JSONObject(myResponse);
-                    jsonArray=jsonObject.getJSONArray("results");
-                    for (int i=0;i<jsonArray.length();i++){
+                    jsonObject = new JSONObject(myResponse);
+                    jsonArray = jsonObject.getJSONArray("results");
+                    for (int i = 0; i < jsonArray.length(); i++) {
 
-                        jsonObject=jsonArray.getJSONObject(i);
-                        String imgBaseUrl="https://image.tmdb.org/t/p/w185"+jsonObject.getString(MOVIE_IMAGE);
+                        jsonObject = jsonArray.getJSONObject(i);
+                        String imgBaseUrl = "https://image.tmdb.org/t/p/w185" + jsonObject.getString(MOVIE_IMAGE);
                         movieModelList.add(new MovieModel(
-                                jsonObject.getString(MOVIE_TITLE),
-                                jsonObject.getString(RELEASE_DATE),
-                                jsonObject.getString(POPULARITY),
-                                jsonObject.getString(VOTE_COUNT),
-                                imgBaseUrl,
-                                jsonObject.getString(VOTE_AVERAGE)
+                                        jsonObject.getString(MOVIE_TITLE),
+                                        jsonObject.getString(RELEASE_DATE),
+                                        jsonObject.getString(POPULARITY),
+                                        jsonObject.getString(VOTE_COUNT),
+                                        imgBaseUrl,
+                                        jsonObject.getString(VOTE_AVERAGE)
+                                )
+                        );
+                    }
+
+                    mainActivity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            publishResult(movieModelList);
+                        }
+                    });
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+
+
+
+    }
+
+    public void getUpcomingMovies() {
+        movieModelList.clear();
+        String url =URL_UPCOMING_MOVIES;
+        request = new Request.Builder().url(url).build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                String myResponse = response.body().string().toString();
+                try {
+                    jsonObject = new JSONObject(myResponse);
+                    jsonArray = jsonObject.getJSONArray("results");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+
+                        jsonObject = jsonArray.getJSONObject(i);
+                        String imgBaseUrl = "https://image.tmdb.org/t/p/w185" + jsonObject.getString(MOVIE_IMAGE);
+                        movieModelList.add(new MovieModel(
+                                        jsonObject.getString(MOVIE_TITLE),
+                                        jsonObject.getString(RELEASE_DATE),
+                                        jsonObject.getString(POPULARITY),
+                                        jsonObject.getString(VOTE_COUNT),
+                                        imgBaseUrl,
+                                        jsonObject.getString(VOTE_AVERAGE)
                                 )
                         );
                     }
@@ -96,26 +151,156 @@ public class MyWebService {
 
     }
 
-    void getLatestMovies(){
-        String url="";
+    public void getLatestMovies() {
+        movieModelList.clear();
+        String url =URL_LATEST_MOVIES;
+        request = new Request.Builder().url(url).build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                String myResponse = response.body().string().toString();
+                try {
+                    jsonObject = new JSONObject(myResponse);
+                    jsonArray = jsonObject.getJSONArray("adult");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+
+                        jsonObject = jsonArray.getJSONObject(i);
+                        String imgBaseUrl = "https://image.tmdb.org/t/p/w185" + jsonObject.getString(MOVIE_IMAGE);
+                        movieModelList.add(new MovieModel(
+                                        jsonObject.getString(MOVIE_TITLE),
+                                        jsonObject.getString(RELEASE_DATE),
+                                        jsonObject.getString(POPULARITY),
+                                        jsonObject.getString(VOTE_COUNT),
+                                        imgBaseUrl,
+                                        jsonObject.getString(VOTE_AVERAGE)
+                                )
+                        );
+                    }
+                    MainActivity mainActivity=new MainActivity();
+                    mainActivity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            publishResult(movieModelList);
+                        }
+                    });
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
     }
 
-    void getNowPlayingMovies(){
-        String url="";
+    public void getNowPlayingMovies() {
+        movieModelList.clear();
+        String url =URL_NOW_PALYING_MOVIES;
+        request = new Request.Builder().url(url).build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                String myResponse = response.body().string().toString();
+                try {
+                    jsonObject = new JSONObject(myResponse);
+                    jsonArray = jsonObject.getJSONArray("results");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+
+                        jsonObject = jsonArray.getJSONObject(i);
+                        String imgBaseUrl = "https://image.tmdb.org/t/p/w185" + jsonObject.getString(MOVIE_IMAGE);
+                        movieModelList.add(new MovieModel(
+                                        jsonObject.getString(MOVIE_TITLE),
+                                        jsonObject.getString(RELEASE_DATE),
+                                        jsonObject.getString(POPULARITY),
+                                        jsonObject.getString(VOTE_COUNT),
+                                        imgBaseUrl,
+                                        jsonObject.getString(VOTE_AVERAGE)
+                                )
+                        );
+                    }
+
+                    mainActivity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            publishResult(movieModelList);
+                        }
+                    });
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+
     }
 
-    void getTopRatedMovies(){
-        String url="";
+    public void getTopRatedMovies()
+    {
+        movieModelList.clear();
+        String url =URL_TOP_RATED_MOVIES;
+        request = new Request.Builder().url(url).build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                String myResponse = response.body().string().toString();
+                try {
+                    jsonObject = new JSONObject(myResponse);
+                    jsonArray = jsonObject.getJSONArray("results");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+
+                        jsonObject = jsonArray.getJSONObject(i);
+                        String imgBaseUrl = "https://image.tmdb.org/t/p/w185" + jsonObject.getString(MOVIE_IMAGE);
+                        movieModelList.add(new MovieModel(
+                                        jsonObject.getString(MOVIE_TITLE),
+                                        jsonObject.getString(RELEASE_DATE),
+                                        jsonObject.getString(POPULARITY),
+                                        jsonObject.getString(VOTE_COUNT),
+                                        imgBaseUrl,
+                                        jsonObject.getString(VOTE_AVERAGE)
+                                )
+                        );
+                    }
+
+                    mainActivity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            publishResult(movieModelList);
+                        }
+                    });
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
     }
 
 
     private void publishResult(List<MovieModel> movieModelList) {
+        MainActivity mainActivity= new MainActivity();
+        recyclerView.setLayoutManager(new LinearLayoutManager(mainActivity));
+        MovieAdapter movieAdapter = new MovieAdapter(movieModelList);
+        movieAdapter.notifyDataSetChanged();
+        recyclerView.setAdapter(movieAdapter);
 
-
-         recyclerView.setLayoutManager(new LinearLayoutManager(mainActivity));
-         MovieAdapter movieAdapter= new MovieAdapter(movieModelList);
-         recyclerView.setAdapter(movieAdapter);
-//       mainActivity.customListViewAdapter = new CustomListViewAdapter(movieAdapters);
-//       rv.setAdapter(new CustomListViewAdapter(MovieAdapter.movieList));
     }
 }
