48311d4e16889f324b81cc12876eca449f7a44de
faintdream <dubey.akash@gmail.com>
2018-03-14 09:20:23 +0530

    Adjusted the listener to MovieDetailsService and overrided from DetailsScreen to pass the move ID back to MovieDetailsService.

--- app/src/main/java/com/akashdubey/imdb/DetailsScreen.java
+++ app/src/main/java/com/akashdubey/imdb/DetailsScreen.java
@@ -6,13 +6,18 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
+import com.akashdubey.imdb.network.MovieDetailsService;
+
+import static com.akashdubey.imdb.network.MovieDetailsService.*;
+
 /**
  * This class loads the UI for details screen and set the movie id for
  * further processing
  */
 
-public class DetailsScreen extends AppCompatActivity {
-    MovieIdListener movieIdListener;
+
+public class DetailsScreen extends AppCompatActivity implements MovieIdListener{
+    MovieIdListener movieIdListener = (MovieIdListener)this ;
     String movieId;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,13 +26,19 @@ public class DetailsScreen extends AppCompatActivity {
         setContentView(R.layout.detailed_view);
         movieId=getIntent().getExtras().getString("movieId");
         movieIdListener.setMovieId(movieId);
+        MovieDetailsService movieDetailsService= new MovieDetailsService();
+        movieDetailsService.getMovieDetail();
 //        Intent intent= new Intent();
 //        Bundle bundle=intent.getExtras();
 
         Toast.makeText(this, "Movie Id "+getIntent().getExtras().getString("movieId"), Toast.LENGTH_LONG).show();
     }
 
-    public interface MovieIdListener{
-        public String setMovieId(String id);
+
+    @Override
+    public void setMovieId(String id) {
+
+        MovieDetailsService.movieId=id;
+
     }
 }
--- app/src/main/java/com/akashdubey/imdb/network/MovieDetailsService.java
+++ app/src/main/java/com/akashdubey/imdb/network/MovieDetailsService.java
@@ -1,61 +1,96 @@
 package com.akashdubey.imdb.network;
 
-import com.akashdubey.imdb.DetailsScreen;
-import com.akashdubey.imdb.DetailsScreen.MovieIdListener;
-
+import android.util.Log;
+import org.json.JSONException;
 import org.json.JSONObject;
+import java.io.IOException;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * Created by homepc on 13-03-2018.
  */
 
-public class MovieDetailsService  implements MovieIdListener{
+public class MovieDetailsService {
 
-    private String movieId="TBD";
+
+    private static final String BUDGET = "budget";
+    private static final String REVENUE = "TBD";
+    private static final String TITLE = "original_title";
+    private static final String OVERVIEW = "overview";
+    private static final String RELEASE_DATE = "TBD";
+    private static final String VOTE_AVERAGE = "TBD";
+
+
+    public static String movieId = "19404";
     private String movieDetailUrl
-            ="http://api.themoviedb.org/3/movie/"+movieId+"?api_key=8496be0b2149805afa458ab8ec27560c";
+            = "http://api.themoviedb.org/3/movie/" + movieId + "?api_key=8496be0b2149805afa458ab8ec27560c";
     private String posterUrl
-            ="http://api.themoviedb.org/3/movie/"+movieId+"/images?api_key=8496be0b2149805afa458ab8ec27560c";
+            = "http://api.themoviedb.org/3/movie/" + movieId + "/images?api_key=8496be0b2149805afa458ab8ec27560c";
     private String castCrewUrl
-            ="http://api.themoviedb.org/3/movie/"+movieId+"/credits?api_key=8496be0b2149805afa458ab8ec27560c";
+            = "http://api.themoviedb.org/3/movie/" + movieId + "/credits?api_key=8496be0b2149805afa458ab8ec27560c";
     private String trailerUrl
-            ="http://api.themoviedb.org/3/movie/"+movieId+"/videos?api_key=8496be0b2149805afa458ab8ec27560c";
+            = "http://api.themoviedb.org/3/movie/" + movieId + "/videos?api_key=8496be0b2149805afa458ab8ec27560c";
 
-    private String dynamicImageURL="TBD.jpeg";
-    private String imageBaseUrl=
-            "http://image.tmdb.org/t/p/w500/"+dynamicImageURL;
+    private String dynamicImageURL = "TBD.jpeg";
+    private String imageBaseUrl =
+            "http://image.tmdb.org/t/p/w500/" + dynamicImageURL;
 
 
     OkHttpClient okHttpClient = new OkHttpClient();
     Request request;
     JSONObject jsonObject;
 
- public void getMovieDetail(){
+    public void getMovieDetail() {
+        String url = movieDetailUrl;
 
-     String url=movieDetailUrl;
+        request = new Request.Builder().url(url).build(); // building the http request okHttp way
+        okHttpClient.newCall(request).enqueue(new Callback() { //sending the request in separate child thread
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String myResponse = response.body().string().toString(); // collecting the result in String object
+                try {
+                    jsonObject = new JSONObject(myResponse);
+
+                    Log.i("LEGO", jsonObject.getString(TITLE));
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
 
- }
- public void getPoster(){
+            }
+        });
+
+    }
+
+    public void getPoster() {
 
 
- }
- public  void getTrailer(){
+    }
 
- }
- public void getCast(){
+    public void getTrailer() {
 
- }
- public void getCrew(){
+    }
 
- }
+    public void getCast() {
 
-    @Override
-    public String setMovieId(String id) {
-        this.movieId=id;
-        return  this.movieId;
+    }
+
+    public void getCrew() {
+
+    }
+
+    public interface MovieIdListener {
+        public void setMovieId(String id);
     }
 }
