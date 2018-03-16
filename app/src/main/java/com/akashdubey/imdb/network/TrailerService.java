44f09a0bbe709c8b7cfec06fcb34d08ce9f8aec4
faintdream <dubey.akash@gmail.com>
2018-03-16 09:00:03 +0530

    added skeleton code for Trailer

---
+++ app/src/main/java/com/akashdubey/imdb/network/TrailerService.java
@@ -0,0 +1,11 @@
+package com.akashdubey.imdb.network;
+
+import com.akashdubey.imdb.DetailsScreen;
+
+/**
+ * this class handles webservice calls for fetching trailer details
+ */
+
+public class TrailerService {
+    DetailsScreen detailsScreen = new DetailsScreen();
+}
